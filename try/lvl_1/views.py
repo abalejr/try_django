33c1c4a84d0542dcbb925d7f30f8c3e72f417ccc
A.J. Bale <abalejr@gmail.com>
2016-09-20 14:57:12 -0500

    Complete Level 2 Section 1

--- try/lvl_1/views.py
+++ try/lvl_1/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # The functions in this file are referred to as VIEWS
 # They all take a request as their input
 
 def index(request):
-	return HttpResponse("<h1>Hello Explorers!</h1>")
+	name = "Gold Nugget"
+	value = 1000.00
+	context = {
+		"treasure_name": name,
+		"treasure_val": value
+	}
+	return render(request, "index.html", context)
